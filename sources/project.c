64a9469a475111f8fe82ffc5f1eb831f5808e632
ggw2329 <ggw2329@naver.com>
2023-11-22 14:42:03 +0900

    func1-3

--- sources/project.c
+++ sources/project.c
@@ -13,7 +13,18 @@ double StartPrice[MONTH][10];//시작가 입력
 double EndPrice[MONTH][10];//종료가 입력
 int Magnification[MONTH][10];//배율 입력
 
-//void CoinProfit(int* CoinPrice) {}
+//double CoinProfit(double* StartPrice,double*EndProce,int i) {
+	//double profit;
+	//profit =(double)(EndPrice[i] / StartPrice[i]) * 100;
+	//return profit;
+//}
+
+//void CoinTypeRate(char*CoinType){}
+
+//void WinRate(int*){}
+//void RecentPnl
+//void TotalPnl
+
 
 int main() {
 
@@ -40,21 +51,29 @@ int main() {
 	sancf_s("%d", &choice);
 
 	if (choice == 1) {
-		printf("해당 거래의 수익률은 %lf%입니다.\n",);
+		int i;
+		printf("몇 번째 거래의 수익률을 볼건지 입력하시오.\n");
+		scanf_s("%d", &i);
+		//printf("해당 거래의 수익률은 %lf%입니다.\n",CoinProfit(StartPrice,EndPrice,i));
 	}
 	else if (choice == 2) {
-		printf("거래시 coin비율은 ");
+		//printf("거래시 coin비율은 ");
 	}
 	else if (choice == 3) {
-		printf("");
+		//printf("내 거래의 성공률은 %.1lf%입니다.\n",WinRate());
 	}
-	
-	
-	printf("몇번째 거래의 수익률을 볼건지 입력하세요.\n");
-	scanf_s("%d",&Trade);
-
-	//printf("%d번째 거래의 수익률은 %lf %입니다.\n",Trade,);
-
-	//CoinProfit(CoinPrice);
+	else if (choice == 4) {
+		//int i;
+		//printf("당일 몇번의 거래를 했는지 입력하시오.\n");
+		//scanf_s("%d",&i);
+		//printf("당일 pnl은 %.1lf입니다.\n",RecentPnl());
+	}
+	else if (choice == 5) {
+		//int i;
+		//printf("총 pnl은 %.1lf입니다.\n",TotalPnl());
+	}
+	else {
+		printf("숫자를 잘못 입력하셨습니다(1~5).\n");
+	}
 	return 0;
 }
