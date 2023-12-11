75b00658e4f153efbbb0330cc19d65575707387b
ggw2329 <ggw2329@naver.com>
2023-12-11 21:35:02 +0900

    진척보고서1210

--- sources/main_project.c
+++ sources/main_project.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MONTH 30
+#define MONTH 1
 #define WEEK 7
 #define DAY 1
 
@@ -8,18 +8,39 @@ int Trade;
 int AssetValue;
 char CoinType[MONTH][10];//종류입력
 char CoinBuySell[MONTH][10];//long,short입력
-int Coinvalue[MONTH][10];//구매량 입력
-double StartPrice[MONTH][10];//시작가 입력
-double EndPrice[MONTH][10];//종료가 입력
-int Magnification[MONTH][10];//배율 입력
+int Coinvalue[MONTH];//구매량 입력
+double StartPrice[MONTH];//시작가 입력
+double EndPrice[MONTH];//종료가 입력
+int Magnification[MONTH];//배율 입력
 
-//double CoinProfit(double* StartPrice,double*EndProce,int i) {
-	//double profit;
-	//profit =(double)(EndPrice[i] / StartPrice[i]) * 100;
-	//return profit;
-//}
+void CoinProfit(double* value1,double* value2,int i,int*Mag) {
+	double profit;
+	if (*value1 <= *value2) {
+		profit = *Mag * ((1-(double)value1[i-1] / (double)value2[i-1])*100);
+		printf("%d번째 거래의 수익률은 %.2lf%c입니다.", i, profit,'%');
+	}
+	else if (*value1 > *value2) {
+		profit = *Mag * (((double)value1[i-1] / (double)value2[i-1]-1) * 100);
+		printf("%d번째 거래의 수익률은 %c %.2lf%c입니다.", i,'-', profit,'%');
+	}
+}
 
-//void CoinTypeRate(char*CoinType){}
+void CoinTypeCount(char*CoinType){
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	for (int i=0; i < MONTH; i++) {
+		if (CoinType[i] == "BTC") {
+			a+=1;
+		}
+		else if (CoinType[i] == "ETH") {
+			b+=1;
+		}
+		else if (CoinType[i] == "XRP") {
+			c+=1;
+		}
+	}printf("BTC:%d번 거래, ETH:%d번 거래, XRP:%d번 거래", a, b, c);
+}
 
 //void WinRate(int*){}
 //void RecentPnl
@@ -28,6 +49,8 @@ int Magnification[MONTH][10];//배율 입력
 
 int main() {
 
+
+
 	printf("계좌의 초기금액을 입력하세요(KRW).\n");
 	scanf_s("%d", &AssetValue);
 
@@ -35,45 +58,55 @@ int main() {
 
 	for (int i = 0; i < MONTH; i++) {
 
-		printf("종료된 %d번째 거래의 정보를 입력하시오.(coin종류(ENG),long/short,구매량(KRW))\n", i + 1);
-		scanf_s("%s %s %d", CoinType[i], (int)sizeof(CoinType),CoinBuySell[i],(int)sizeof(CoinBuySell),Coinvalue[i]);
-		getchar();
+		printf("종료된 %d번째 거래의 정보를 입력하시오.(coin종류(BTC,ETH,XRP),long/short,구매량(KRW))\n", i + 1);
+		scanf_s("%s %s %d", CoinType[i], (int)sizeof(CoinType),CoinBuySell[i],(int)sizeof(CoinBuySell),&Coinvalue[i]);
 		printf("종료된 %d번째 거래의 정보를 입력하시오.(시작가,종료가,배율)\n", i + 1);
-		scanf_s("%lf %lf %d",StartPrice[i],EndPrice[i],Magnification[i]);
+		scanf_s("%lf %lf %d",&StartPrice[i],&EndPrice[i],&Magnification[i]);
 	}
 	
 	int choice;
-	printf("1.해당 거래의 수익률");
-	printf("2.coin종류당 비율");
-	printf("3.내 거래 성공률");
-	printf("4.당일 pnl보기");
-	printf("5.total pnl 보기");
-	sancf_s("%d", &choice);
+	printf("\n1.해당 거래의 수익률");
+	printf("\n2.coin종류당 거래횟수");
+	printf("\n3.내 거래 성공률");
+	printf("\n4.당일 pnl보기");
+	printf("\n5.total pnl 보기\n");
+	printf("0.프로그램 종료\n");
 
-	if (choice == 1) {
-		int i;
-		printf("몇 번째 거래의 수익률을 볼건지 입력하시오.\n");
-		scanf_s("%d", &i);
-		//printf("해당 거래의 수익률은 %lf%입니다.\n",CoinProfit(StartPrice,EndPrice,i));
-	}
-	else if (choice == 2) {
-		//printf("거래시 coin비율은 ");
-	}
-	else if (choice == 3) {
-		//printf("내 거래의 성공률은 %.1lf%입니다.\n",WinRate());
-	}
-	else if (choice == 4) {
-		//int i;
-		//printf("당일 몇번의 거래를 했는지 입력하시오.\n");
-		//scanf_s("%d",&i);
-		//printf("당일 pnl은 %.1lf입니다.\n",RecentPnl());
-	}
-	else if (choice == 5) {
-		//int i;
-		//printf("총 pnl은 %.1lf입니다.\n",TotalPnl());
-	}
-	else {
-		printf("숫자를 잘못 입력하셨습니다(1~5).\n");
+
+	for (int i = 0; i < 100; i++) {
+		printf("\n이용할 옵션을 선택하세요.(0~5)");
+		scanf_s("%d", &choice);
+		printf("%d번째 옵션을 선택하셨습니다.\n", choice);
+		if (choice == 1) {
+			int i;
+			printf("몇 번째 거래의 수익률을 볼건지 입력하시오.\n");
+			scanf_s("%d", &i);
+			CoinProfit(StartPrice, EndPrice, i,Magnification);
+		}
+		else if (choice == 2) {
+			printf("coin종류당 거래횟수는 다음과 같습니다.\n");
+			CoinTypeCount(*CoinType);
+		}
+		else if (choice == 3) {
+			//printf("내 거래의 성공률은 %.1lf%입니다.\n",WinRate());
+		}
+		else if (choice == 4) {
+			//int i;
+			//printf("당일 몇번의 거래를 했는지 입력하시오.\n");
+			//scanf_s("%d",&i);
+			//printf("당일 pnl은 %.1lf입니다.\n",RecentPnl());
+		}
+		else if (choice == 5) {
+			//int i;
+			//printf("총 pnl은 %.1lf입니다.\n",TotalPnl());
+		}
+		else if (choice == 0) {
+			printf("프로그램을 종료합니다.\n");
+			break;
+		}
+		else {
+			printf("숫자를 잘못 입력하셨습니다(0~5).\n");
+		}
 	}
 	return 0;
 }
