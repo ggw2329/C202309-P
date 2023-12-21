ba736005a898e964b921f79459f70f833bc2684a
ggw2329 <ggw2329@naver.com>
2023-12-21 11:52:29 +0900

    modifying fucn6,etc.

--- sources/main_project.c
+++ sources/main_project.c
@@ -5,7 +5,9 @@
 #define WEEK 7
 #define DAY 1
 
-
+// 배열들 구조체로 변경
+// (배열들 동적메모리 변경->define 삭제)
+// 함수들 헤더파일로 변경
 int Trade;
 int AssetValue;
 char CoinType[MONTH][10];//종류입력
@@ -15,6 +17,7 @@ double StartPrice[MONTH];//시작가 입력
 double EndPrice[MONTH];//종료가 입력
 int Magnification[MONTH];//배율 입력
 
+//롱숏 조건 추가
 void CoinProfit(double* value1,double* value2,int i,int*Mag) {
 	double profit;
 	if (*value1 <= *value2) {
@@ -32,18 +35,19 @@ void CoinTypeCount(char*CoinType){
 	int b = 0;
 	int c = 0;
 	for (int i=0; i < MONTH; i++) {
-		if (CoinType[i] == "BTC") {
+		if (CoinType[i] == 'B') {
 			a+=1;
 		}
-		else if (CoinType[i] == "ETH") {
+		else if (CoinType[i] == 'E') {
 			b+=1;
 		}
-		else if (CoinType[i] == "XRP") {
+		else if (CoinType[i] == 'X') {
 			c+=1;
 		}
 	}printf("BTC:%d번 거래, ETH:%d번 거래, XRP:%d번 거래", a, b, c);
 }
 
+//롱숏 조건 추가
 void WinRate(double*SP,double*EP){
 	int a=0;
 	for (int i = 0; i < MONTH; i++) {
@@ -51,7 +55,7 @@ void WinRate(double*SP,double*EP){
 			a++;
 		}
 	}
-	printf("고객님의 거래 승률은 %d%c입니다", a / MONTH * 100, '%');
+	printf("고객님의 총거래 승률은 %d%c입니다", a / MONTH * 100, '%');
 }
 //void RecentPnl
 //void TotalPnl
@@ -68,7 +72,7 @@ int main() {
 
 	for (int i = 0; i < MONTH; i++) {
 
-		printf("종료된 %d번째 거래의 정보를 입력하시오.(coin종류(BTC,ETH,XRP),long/short,구매량(KRW))\n", i + 1);
+		printf("종료된 %d번째 거래의 정보를 입력하시오.(coin종류(BTC,ETH,XRP),롱/숏(L,S),구매량(KRW))(영문입력시 앞글자만 입력)\n", i + 1);
 		scanf_s("%s %s %d", CoinType[i], (int)sizeof(CoinType),CoinBuySell[i],(int)sizeof(CoinBuySell),&Coinvalue[i]);
 		printf("종료된 %d번째 거래의 정보를 입력하시오.(시작가,종료가,배율)\n", i + 1);
 		scanf_s("%lf %lf %d",&StartPrice[i],&EndPrice[i],&Magnification[i]);
@@ -79,7 +83,8 @@ int main() {
 	printf("\n2.coin종류당 거래횟수");
 	printf("\n3.내 거래 성공률");
 	printf("\n4.당일 pnl보기");
-	printf("\n5.total pnl 보기\n");
+	printf("\n5.total pnl 보기");
+	printf("\n6.asset value 보기\n");
 	printf("0.프로그램 종료\n");
 
 
@@ -109,6 +114,10 @@ int main() {
 		else if (choice == 5) {
 			//int i;
 			//printf("총 pnl은 %.1lf입니다.\n",TotalPnl());
+		}
+		else if (choice == 6) {
+			//assetvalue= 처음value+value*수익률
+			//printf("현재 assetvalue는 %d입니다.\n",Assetvalue);
 		}
 		else if (choice == 0) {
 			printf("프로그램을 종료합니다.\n");
